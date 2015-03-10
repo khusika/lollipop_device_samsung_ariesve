d79e11cfdebbb0768a617485c3a0ca5befb5f568
khusika <khusikadhamar@gmail.com>
2015-03-11 06:58:33 +0700

    Fix derp

--- liquid.mk
+++ liquid.mk
@@ -4,17 +4,10 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ariesve.mk)
 
-<<<<<<< HEAD:liquid.mk
 # boot animation
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
 
-# This device is hdpi
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-=======
->>>>>>> 071f434f35283df76bc68b8153e635063d868bb2:cm.mk
 PRODUCT_RELEASE_NAME := ariesve
 
 # Setup device configuration
